be4677dcc7dfd196278e6c1f8934458854b7a401
shiraishihayato1031 <tukuyomi1031@outlook.jp>
2024-12-26 23:05:01 +0900

    配列から要素を抽出して、教科ごとや一人目などの平均値を出すことができません、どうすればよいでしょうか？

--- src/curriculum_B/Qes7.java
+++ src/curriculum_B/Qes7.java
@@ -1,12 +1,57 @@
 package curriculum_B;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Qes7 {
 	public static void main(String[] args) {
+		
 	Scanner stbin = new Scanner(System.in);
+		
+	System.out.print("生徒の人数を入力してください(2人以上) :");
 	
-	Scanner score = stbin,nextInt();
+	int n = stbin.nextInt();
+	
+	int i = 1;
+	
+	System.out.println(n);
+	
+	ArrayList<Integer> num = new ArrayList<Integer>();
+	
+	while(i <= n) {
+		
+		System.out.print(i+"人目の『英語』の点数を入力してください:");
+		int v = stbin.nextInt();
+		num.add(v);
+
+		System.out.print(i+"人目の『数学』の点数を入力してください:");
+		 v = stbin.nextInt();
+		 num.add(v);
+		
+		System.out.print(i+"人目の『理科』の点数を入力してください:");
+		 v = stbin.nextInt();
+		 num.add(v);
+		
+		System.out.print(i+"人目の『社会』の点数を入力してください:");
+        v = stbin.nextInt();
+        num.add(v);
+        
+
+		}
+	
+	for(int x = 0; i < num.length; i ++) {
+		System.out.println((x + 1) + "人目のスコアは " + num[x]);
+	
+        System.out.println("");
+        
+        System.out.println(num);
+        
+        System.out.plintln()
+        
+        i++;
+	};
+	
+	
 	
 	stbin.close();
 	}
